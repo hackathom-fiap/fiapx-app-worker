c01c33e0f6287afacf1089fa653ee2bcecf6c998
hmouraf <humbertomoura@gmail.com>
2026-03-10 22:36:02 -0300

    fix processor sonar cobertura

--- src/test/java/com/fiapx/processor/infrastructure/adapter/email/EmailNotificationAdapterTest.java
+++ src/test/java/com/fiapx/processor/infrastructure/adapter/email/EmailNotificationAdapterTest.java
@@ -1,5 +1,6 @@
 package com.fiapx.processor.infrastructure.adapter.email;
 
+import com.fiapx.processor.infrastructure.adapter.email.EmailNotificationAdapter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
